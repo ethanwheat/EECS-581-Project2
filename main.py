defc4202c6d9efc45d53f75c5472ebdae195c33b
Tyler51235 <155043620+Tyler51235@users.noreply.github.com>
2024-09-11 16:40:29 -0500

    Adequate prologue

---
+++ main.py
@@ -0,0 +1,8 @@
+'''
+Battleship
+This program allows its users to be able to play battleships with up to 5 ships
+Inputs are the placement of ships and shooting at ships
+Outputs are "hit" or "miss"
+Authors: Kyler Luong, 
+Date: 9/11/24
+ '''
