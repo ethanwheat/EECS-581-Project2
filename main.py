8024f2201da1d22ad57689ed3eb905e41171e766
Tyler51235 <155043620+Tyler51235@users.noreply.github.com>
2024-09-11 16:46:26 -0500

    Creating the 10 x 10 grid

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ Outputs are "hit" or "miss"
 Authors: Kyler Luong, 
 Date: 9/11/24
  '''
+
+def create_grid(row, col):
+    grid = [[0 for _ in range(col)] for _ in range(row)]
+    return grid
+grid = create_grid(10,10)
+for row in grid:
+    print(row)
