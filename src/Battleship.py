9cb23544d0ae707a897b2283722dc96f93e76195
Gavin Kirwan <gavin.kirwan@icloud.com>
2024-09-25 22:56:38 -0500

    Update prompts

--- src/Battleship.py
+++ src/Battleship.py
@@ -33,7 +33,7 @@ class Battleship:
           cols (int, optional): The number of columns in this Battleship game. Defaults to `10`
         """
 
-        # Prompt setup game
+        # Prompt setup gam
         option, self.difficulty, numShips = self.setupGame()
         playingAI = option == "2"
 
